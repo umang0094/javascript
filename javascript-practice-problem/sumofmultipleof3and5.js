a3d045253b97a0c07d88663e6c3968da687e418a
unknown <umang0094@gmail.com>
2021-09-23 09:47:54 +0530

    solution of sum of all multiple of 3 and 5

--- javascript-practice-problem/sumofmultipleof3and5.js
+++ javascript-practice-problem/sumofmultipleof3and5.js
@@ -1,3 +1,7 @@
+
+
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
 function multiplesOf3and5(number) {
     var sum = 0;
     for(let i = 1; i < number;i++){
@@ -9,4 +13,4 @@ function multiplesOf3and5(number) {
     return sum;
   }
   
-  console.log(multiplesOf3and5(10));
+  console.log(multiplesOf3and5(1000));
